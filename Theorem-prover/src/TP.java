51dbf14e392880fac369421d13319fabbd5bb9a5
Pratyaksh <pratyaksh@me.com>
2015-02-09 16:41:43 +0530

    Theorem-prover: Added human help

--- Theorem-prover/src/TP.java
+++ Theorem-prover/src/TP.java
@@ -12,13 +12,15 @@ public class TP {
 	static HashMap<String, parseTree> map = new HashMap<String,parseTree>();
 	static HashMap<String, parseTree> map2 = new HashMap<String,parseTree>();
 	static boolean found=false;
+	static List<parseTree> lhs_tree_orig = new ArrayList<parseTree>();
+	static HashMap<String, parseTree> map_orig = new HashMap<String,parseTree>();
 
 
 	static boolean mod_pon(parseTree T1, parseTree T2){
 		boolean b = T1.compare(T2.ltree);
 		if (b == true) {
-//			System.out.println(parseTree.print_tree(T1));
-//			System.out.println(parseTree.print_tree(T2));
+			//			System.out.println(parseTree.print_tree(T1));
+			//			System.out.println(parseTree.print_tree(T2));
 		}
 		return b;
 	}
@@ -44,21 +46,21 @@ public class TP {
 				{
 					parseTree ins=new parseTree(lhs_tree.get(j).rtree);
 					String k=parseTree.print_tree(ins);
-					map2.put(k,ins);
+					//map2.put(k,ins);
 					Add_pt(ins);///
 					k=parseTree.print_tree(lhs_tree.get(j));
-					map2.put(k,lhs_tree.get(j));
+					//map2.put(k,lhs_tree.get(j));
 				}
 				if(mod_pon(lhs_tree.get(j),lhs_tree.get(i)))
 				{
 					parseTree ins=new parseTree(lhs_tree.get(i).rtree);
 					String k=parseTree.print_tree(ins);
-					map2.put(k,ins);
+					//map2.put(k,ins);
 					Add_pt(ins);///
 					k=parseTree.print_tree(lhs_tree.get(j));
-					map2.put(k,lhs_tree.get(j));
+					//map2.put(k,lhs_tree.get(j));
 					k=parseTree.print_tree(lhs_tree.get(i));
-					map2.put(k,lhs_tree.get(i));
+					//map2.put(k,lhs_tree.get(i));
 				}
 			}
 		}
@@ -247,19 +249,19 @@ public class TP {
 			parseTree A = subtrees.get(key);
 			parseTree T3 = parseTree.A3(A);
 			new_map.put(parseTree.print_tree(T3), T3);
-//			System.out.println("A3 on " + parseTree.print_tree(A) + " gives " + parseTree.print_tree(T3));
+			//			System.out.println("A3 on " + parseTree.print_tree(A) + " gives " + parseTree.print_tree(T3));
 			for (String key2 : subtrees.keySet()) 
 			{
 				parseTree B = subtrees.get(key2);
 				parseTree T1 = parseTree.A1(A, B);
 				new_map.put(parseTree.print_tree(T1), T1);
-//				System.out.println("A1 on " + parseTree.print_tree(A) + " and " + parseTree.print_tree(B) + " gives " + parseTree.print_tree(T1));
+				//				System.out.println("A1 on " + parseTree.print_tree(A) + " and " + parseTree.print_tree(B) + " gives " + parseTree.print_tree(T1));
 				for (String key3 : subtrees.keySet()) 
 				{
 					parseTree C = subtrees.get(key3);
 					parseTree T2 = parseTree.A2(A, B, C);
 					new_map.put(parseTree.print_tree(T2), T2);
-//					System.out.println("A2 on " + parseTree.print_tree(A) + " and " + parseTree.print_tree(B) + " and " + parseTree.print_tree(C) + " gives " + parseTree.print_tree(T2));
+					//					System.out.println("A2 on " + parseTree.print_tree(A) + " and " + parseTree.print_tree(B) + " and " + parseTree.print_tree(C) + " gives " + parseTree.print_tree(T2));
 				}
 			}
 		}
@@ -276,10 +278,10 @@ public class TP {
 
 	public static void main(String[] args) {
 
-//		Scanner input = new Scanner(System.in);
-//		String line = input.nextLine();
-		
-		String line = "(p|(p&q))->p";
+		Scanner input = new Scanner(System.in);
+		String line = input.nextLine();
+
+		//		String line = "(p|(p&q))->p";
 		line = reduceAND(reduceOR(reduceNOT(line)));
 		System.out.println("line = " + line);
 		moveLeft(line);
@@ -289,18 +291,80 @@ public class TP {
 		System.out.println("----");
 		System.out.println(lhs.get(lhs.size()-1));
 
-		int i=0;
-		
-//		for(i = 0; i < lhs_tree.size(); i++)
-//			System.out.println(parseTree.print_tree(lhs_tree.get(i)));
-//		parseTree t = new parseTree();
-//		parseTree.create_pt(t, "(p->(q->F))->F");
-//		System.out.println(parseTree.print_tree(t));
-		
-		while(!found) {
-			applyAxioms();
-			applymp();
-			System.out.println(lhs_tree.size());
+		// store the initial propositions
+		lhs_tree_orig = new ArrayList<parseTree>(lhs_tree);
+		map_orig = new HashMap<String, parseTree>(map);
+
+		//		for(i = 0; i < lhs_tree.size(); i++)
+		//			System.out.println(parseTree.print_tree(lhs_tree.get(i)));
+		//		parseTree t = new parseTree();
+		//		parseTree.create_pt(t, "(p->(q->F))->F");
+		//		System.out.println(parseTree.print_tree(t));
+
+		int i = 0;
+		while (!found){
+			i = 0;
+			while(!found && i++ < 1) {
+				applyAxioms();
+				applymp();
+				System.out.println(lhs_tree.size());
+			}
+			if (!found) {
+				System.out.println("It seems that the ATP is stuck and need some human intervention. Press 1 to Apply Axiom 1, 2 to apply Axiom 2, 3 to apply axiom 3 and 4 to use a new theorem");
+				String ans=input.nextLine();
+				parseTree ins=new parseTree();
+				if(ans.equals("1"))
+				{
+					String a=input.nextLine();
+					String b=input.nextLine();
+					a = reduceAND(reduceOR(reduceNOT(a)));
+					b = reduceAND(reduceOR(reduceNOT(b)));
+					parseTree A=new parseTree();
+					parseTree.create_pt(A,a);
+					parseTree B=new parseTree();
+					parseTree.create_pt(B,b);
+
+					ins=parseTree.A1(A,B);
+				}
+				else if(ans.equals("2"))
+				{
+					String a=input.nextLine();
+					String b=input.nextLine();
+					String c=input.nextLine();
+					a = reduceAND(reduceOR(reduceNOT(a)));
+					b = reduceAND(reduceOR(reduceNOT(b)));
+					c = reduceAND(reduceOR(reduceNOT(c)));
+					parseTree A=new parseTree();
+					parseTree.create_pt(A,a);
+					parseTree B=new parseTree();
+					parseTree.create_pt(B,b);
+					parseTree C=new parseTree();
+					parseTree.create_pt(C,c);
+					ins=parseTree.A2(A,B,C);
+				}
+				else if(ans.equals("3"))
+				{
+					String a=input.nextLine();
+					parseTree A=new parseTree();
+					parseTree.create_pt(A,a);
+					ins=parseTree.A3(A);
+				}
+				else if(ans.equals("4"))
+				{
+					String a=input.nextLine();
+					parseTree.create_pt(ins,a);
+				}
+				
+				String k = parseTree.print_tree(ins);
+				if(map_orig.get(k)==null) 
+				{
+					//			System.out.println(k +" : inserted");
+					map_orig.put(k,ins);
+					lhs_tree_orig.add(ins);
+				}
+				lhs_tree = new ArrayList<parseTree>(lhs_tree_orig);
+				map = new HashMap<String, parseTree>(map_orig);
+			}
 		}
 	}
 
